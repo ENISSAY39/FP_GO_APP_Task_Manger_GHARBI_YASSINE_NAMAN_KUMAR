c50dfeef7f10c6a3d4370748cd5f2bc10e643681
ENISSAY39 <128503922+ENISSAY39@users.noreply.github.com>
2025-12-02 16:08:42 +0700

    Resolve merge conflict in UserModel.go

--- models/UserModel.go
+++ models/UserModel.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package models
 
 import "gorm.io/gorm"
@@ -12,18 +11,3 @@ type User struct {
 	Tasks    []Task         `gorm:"many2many:task_assignees" json:"-"`
 	Members  []ProjectMember `gorm:"foreignKey:UserID" json:"-"`
 }
-=======
-package models
-
-import "gorm.io/gorm"
-
-// User represents an application user
-type User struct {
-	gorm.Model
-	Email    string         `gorm:"unique;not null" json:"email"`
-	Password string         `gorm:"not null" json:"-"`
-	Projects []Project      `gorm:"foreignKey:OwnerID" json:"-"`
-	Tasks    []Task         `gorm:"many2many:task_assignees" json:"-"`
-	Members  []ProjectMember `gorm:"foreignKey:UserID" json:"-"`
-}
->>>>>>> 59b3e1a700bc3a3e68597e88c6d5bf8511614e7c
